29bc968a183d631864f206c7ef76641878fe6d86
kbi <kbi@peytz.dk>
2021-01-27 22:54:23 +0100

    seo

--- pages/test-test/[id].js
+++ pages/test-test/[id].js
@@ -1,5 +1,9 @@
 const Product = ({ productId }) => {
-  return <> This is my product id {productId} </>;
+  return (
+    <>
+      <h1>{`This is my product id ${productId}`}</h1>{" "}
+    </>
+  );
 };
 Product.getInitialProps = async (ctx) => {
   return {

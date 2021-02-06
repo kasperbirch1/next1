60d4040d8a9d0519a09a20f1f91c94183cdf1aac
kbi <kbi@peytz.dk>
2021-02-06 19:05:14 +0100

    updated

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,28 +1,20 @@
-import { seoByer } from "../../../seoByer.js";
 import Cors from "cors";
-import initMiddleware from "../../../lib/init-middleware";
+import initMiddleware from "../../lib/init-middleware";
+import { seoByer } from "../../seoByer.js";
 
 // Initialize the cors middleware
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods: ["GET"],
+    methods: ["GET", "POST", "OPTIONS"],
   })
 );
 
 export default async function handler(req, res) {
-  const result = seoByer.filter(
-    (city) => city.name == req.query.byId.toLowerCase()
-  );
-
   // Run cors
   await cors(req, res);
 
   // Rest of the API logic
-  if (result.length > 0) {
-    res.status(200).json(result[0]);
-  } else {
-    res.status(200).json({ name: req.query.byId });
-  }
+  res.json(seoByer);
 }

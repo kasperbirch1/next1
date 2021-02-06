cfb0f5c7cf8cad59aa5b7ea55bc40588b23d209d
kbi <kbi@peytz.dk>
2021-02-06 20:31:51 +0100

    update

--- pages/bestil-laasesmed/index.js
+++ pages/bestil-laasesmed/index.js
@@ -1,8 +1,9 @@
 import asyncComponent from "../../components/asyncComponent";
 // import GoogleMaps from "../../components/GoogleMapsPlaces";
 import Meta from "../../layout/Meta";
+import Head from "next/head";
 
-const AsyncFrontPage = asyncComponent(() =>
+const AsyncGoogleMaps = asyncComponent(() =>
   import("../../components/GoogleMapsPlaces")
 );
 
@@ -10,9 +11,15 @@ const index = () => {
   return (
     <>
       <Meta title="| Bestil Låsesmed" />
+      <Head>
+        <script
+          defer
+          type="text/javascript"
+          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCg1FoFelKpnmBn_sx2GUZtA-SI6HCPMiU&libraries=places"
+        ></script>
+      </Head>
       <h1>Bestil en låsesmed</h1>
-
-      <AsyncFrontPage />
+      <AsyncGoogleMaps />
     </>
   );
 };

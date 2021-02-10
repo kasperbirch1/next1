7bb2a844a1dcc0556525e4989914395c940adaa0
kbi <kbi@peytz.dk>
2021-02-10 07:23:45 +0100

    remove buttons

--- components/CardList/CardItem.js
+++ components/CardList/CardItem.js
@@ -33,14 +33,14 @@ const CardItem = ({ title, description, image }) => {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      {/* <CardActions>
         <Button size="small" color="primary">
           Share
         </Button>
         <Button size="small" color="primary">
           Learn More
         </Button>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 };

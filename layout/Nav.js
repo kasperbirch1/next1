70efd4d9fe93815e058424bbbb43faf680f9a580
kbi <kbi@peytz.dk>
2021-03-02 12:58:03 +0100

    Add: modal menu

--- layout/Nav.js
+++ layout/Nav.js
@@ -4,48 +4,84 @@ import { Button } from "@material-ui/core";
 import PhoneIcon from "@material-ui/icons/Phone";
 import MenuIcon from "@material-ui/icons/Menu";
 import { useState } from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement(`#__next`);
+
+const modalStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  content: {
+    top: "0",
+    right: "0",
+    // bottom: "0",
+    // backgroundColor: "rgba(255, 255, 255, .9)",
+    width: "100%",
+    maxWidth: "500px",
+    padding: "2rem",
+    marginLeft: "auto",
+    inset: "0px",
+  },
+};
 
 const Nav = () => {
-  const [togglemenu, setTogglemenu] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
-    <nav className={navStyles.nav}>
-      <div className={`wrapper ${navStyles.mobile}`}>
-        <Button
-          onClick={() => {
-            setTogglemenu(!togglemenu);
-          }}
-          variant="contained"
-          startIcon={<MenuIcon />}
-          color="primary"
-        >
-          Menu
-        </Button>
-        <a href="tel:+4531313515">
-          <Button variant="contained" startIcon={<PhoneIcon />} color="primary">
-            31 31 35 15
+    <>
+      <nav className={navStyles.nav}>
+        <div className={`wrapper ${navStyles.mobile}`}>
+          <Button
+            onClick={() => {
+              setModalOpen(true);
+            }}
+            variant="contained"
+            startIcon={<MenuIcon />}
+            color="primary"
+          >
+            Menu
           </Button>
-        </a>
-      </div>
-
-      <div className={`wrapper ${navStyles.desktop}`}>
-        <MenuList addPhone />
-      </div>
-
-      {togglemenu && (
-        <div className={navStyles.destopMenuListContainer}>
-          <MenuList changeFlex setTogglemenu={setTogglemenu} />
+          <a href="tel:+4531313515">
+            <Button
+              variant="contained"
+              startIcon={<PhoneIcon />}
+              color="primary"
+            >
+              31 31 35 15
+            </Button>
+          </a>
         </div>
-      )}
-    </nav>
+      </nav>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={closeModal}
+        style={modalStyles}
+        closeTimeoutMS={500}
+      >
+        <p
+          onClick={closeModal}
+          style={{
+            fontSize: "1.5rem",
+            position: "absolute",
+            top: "1.5rem",
+            right: "1.5rem",
+          }}
+        >
+          Close
+        </p>
+        <MenuList />
+      </Modal>
+    </>
   );
 };
 
-export const MenuList = ({ addPhone, changeFlex, footer }) => {
+export const MenuList = ({ addPhone }) => {
   return (
-    <ul
-      className={changeFlex ? navStyles.changeFlexMenuList : navStyles.MenuList}
-    >
+    <ul className={navStyles.MenuList}>
       <li>
         <Link href="/">Forside</Link>
       </li>

44140ea4a77a09cbb4e41430d13453f64759a6be
kbi <kbi@peytz.dk>
2021-03-09 17:19:50 +0100

    Change uppercase folder

--- components/CardList/index.js
+++ components/CardList/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CardItem from "./CardItem";
-import { serviceItems } from "../../Content/serviceItems";
-import styled from 'styled-components'
+import { serviceItems } from "../../content/serviceItems";
+import styled from "styled-components";
 const StyledCardListSection = styled.section`
   display: grid;
   grid-gap: 1rem;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-`
+`;
 
 const CardList = () => {
   return (
